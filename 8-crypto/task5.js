ea6c3d13dd16dff5ba5f3cf43bf4a62c06742943
Sergey Rogov <s.a.rogov20@gmail.com>
2024-07-22 13:28:41 +0300

    Фиксы

---
+++ 8-crypto/task5.js
@@ -0,0 +1,25 @@
+let originalString = 'password';
+console.log(originalString);
+console.log(Crypto(originalString));
+console.log(Check(Crypto(originalString)));
+
+
+function Crypto(str) {
+    return TwoPointers(str);
+}
+
+function Check(str) {
+    return TwoPointers(str);
+}
+
+function TwoPointers(str) {
+    let char = str.split('');
+    let left = 0;
+    let right = char.length - 1;
+    while (left < right) {
+        [char[left], char[right]] = [char[right], char[left]];
+        left++;
+        right--;
+    }
+    return char.join('')
+}
